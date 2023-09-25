82063389b6eb393bc9932db016438c2319dbd83f
Ivan <iva.kot1331@gmail.com>
2023-09-25 21:22:35 +0300

    Добавление элемента портфолио

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,6 +3,6 @@ const openButtons = document.querySelectorAll(".portfolio__elem__preview")
 openButtons.forEach((openButton) => {
     openButton.addEventListener("click", () => {
         openButton.parentElement.querySelector(".popup-wrapper").classList.add("visible")
-        document.body.classList.remove('overflow-hidden');
+        document.body.classList.add('overflow-hidden');
     })
 })
