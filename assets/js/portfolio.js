fb0a64cbf2fd2b4178aef5df6bc162e84e2a867f
Ivan <iva.kot1331@gmail.com>
2024-01-13 21:54:43 +0300

    Изменение дизайна

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,8 +1,21 @@
-const openButtons = document.querySelectorAll(".portfolio__elem__preview")
+const openButtons = document.querySelectorAll(".portfolio__elem")
 
 openButtons.forEach((openButton) => {
     openButton.addEventListener("click", () => {
         openButton.parentElement.querySelector(".popup-wrapper").classList.add("visible")
         document.body.classList.add('overflow-hidden');
+    })
+})
+
+
+
+const portfolioItems = document.querySelectorAll(".portfolio__elem")
+
+portfolioItems.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.classList.add("active")
+    })
+    item.addEventListener("mouseout", () => {
+        item.classList.remove("active")
     })
 })
