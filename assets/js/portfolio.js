be3cfb8443fa6bc1e8dd3c260491b5cd617b09c1
Ivan <iva.kot1331@gmail.com>
2024-01-17 18:23:33 +0300

    Редизайн элементов портфолио

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,21 +1,13 @@
-const openButtons = document.querySelectorAll(".portfolio__elem")
-
-openButtons.forEach((openButton) => {
-    openButton.addEventListener("click", () => {
-        openButton.parentElement.querySelector(".popup-wrapper").classList.add("visible")
-        document.body.classList.add('overflow-hidden');
-    })
-})
-
-
-
 const portfolioItems = document.querySelectorAll(".portfolio__elem")
 
-portfolioItems.forEach(item => {
-    item.addEventListener("mouseover", () => {
-        item.classList.add("active")
-    })
-    item.addEventListener("mouseout", () => {
-        item.classList.remove("active")
+portfolioItems.forEach((portfolioItem) => {
+    console.log(portfolioItem)
+    const button = portfolioItem.querySelector(".portfolio__elem__button")
+    const popup = portfolioItem.querySelector(".popup-wrapper")
+    button.addEventListener("click", () => {
+        console.log(button)
+        console.log(popup)
+        popup.classList.add("visible")
+        document.body.classList.add('overflow-hidden');
     })
 })
