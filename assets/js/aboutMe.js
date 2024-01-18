3142a4bdba1a5895e6fb85bec811bd652bd2d66b
Ivan <iva.kot1331@gmail.com>
2024-01-18 16:09:44 +0300

    Изменение скрипта блока обо мне

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -1,33 +1,48 @@
-const aboutMeItems = document.querySelectorAll('.about-me__subtitle p');
+const aboutMeText = document.querySelector('.about-me__subtitle__text');
 
-aboutMeItems.forEach((item) => {
-  animateText(item);
-});
+animateText(aboutMeText);
 
 function animateText(elem) {
   const text = elem.innerText;
+  console.log(elem)
 
-  elem.innerText = '';
+  elem.innerHTML = '';
 
   const splitedText = text.split('');
+  console.log(splitedText)
 
-  window.addEventListener('scroll', function () {
-    const aboutMe = document.querySelectorAll('.about-me')[0].getBoundingClientRect().top;
+  window.addEventListener('scroll', windowScrollHandler);
+
+  function windowScrollHandler() {
+    const aboutMe = document.querySelector('.about-me').getBoundingClientRect().top;
 
     if (aboutMe <= 300) {
-      if (elem.classList.length == 0) {
+
+      if (elem.classList.length == 1) {
         elem.classList.add('active');
+
         splitedText.forEach((letter, i) => {
           setTimeout(() => {
-            if (letter == ' ') {
-              elem.innerHTML = elem.innerHTML + ' ';
+
+            if(letter === "#") {
+              elem.innerHTML = elem.innerHTML + "<br>";
             } else {
               elem.innerHTML = elem.innerHTML + letter;
             }
 
-          }, i * 50);
+            if(i === splitedText.length - 1) {
+              const caret = document.querySelector(".caret")
+              caret.classList.add("disabled")
+            }
+
+          }, i * 45);
         });
+
       }
+
+      window.removeEventListener("scroll", windowScrollHandler)
     }
-  });
+  }
 }
+
+
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,12 +1,9 @@
 const portfolioItems = document.querySelectorAll(".portfolio__elem")
 
 portfolioItems.forEach((portfolioItem) => {
-    console.log(portfolioItem)
     const button = portfolioItem.querySelector(".portfolio__elem__button")
     const popup = portfolioItem.querySelector(".popup-wrapper")
     button.addEventListener("click", () => {
-        console.log(button)
-        console.log(popup)
         popup.classList.add("visible")
         document.body.classList.add('overflow-hidden');
     })
